7113a040e3a5814c8a0a32428d6f5fb36652dbf7
Osama Nooruldin Faqhruldin <onfaqhruldin@uwaterloo.ca>
2019-11-13 21:11:57 -0500

    Updates

---
+++ precache-manifest.509b5969d06498680d9fe357aee47aaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedc544f2b44ec2569ba46f5b4251969",
+    "url": "/webpage_resume/index.html"
+  },
+  {
+    "revision": "06af51bf15a96256c86b",
+    "url": "/webpage_resume/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "06af51bf15a96256c86b",
+    "url": "/webpage_resume/static/js/2.58de20d5.chunk.js"
+  },
+  {
+    "revision": "7afb3792c3dde2afcb3c",
+    "url": "/webpage_resume/static/js/main.76e94101.chunk.js"
+  },
+  {
+    "revision": "bad6e6582adf1e4518d5",
+    "url": "/webpage_resume/static/js/runtime-main.1cb3afb8.js"
+  },
+  {
+    "revision": "bb8195948ebaa60bff4189d5dafa0412",
+    "url": "/webpage_resume/static/media/button.bb819594.svg"
+  },
+  {
+    "revision": "3aed38bc9fdf166619db099af10aee12",
+    "url": "/webpage_resume/static/media/profile_pic.3aed38bc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dd7d33ee23b687e492d1380f4dab1c6.js"
+  "/webpage_resume/precache-manifest.509b5969d06498680d9fe357aee47aaf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webpage_resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
